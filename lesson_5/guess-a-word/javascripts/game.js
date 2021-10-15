040e03c53924fa8a0f3f9f71861e5ed51c0057ad
Raul Romero Cano <raulromerocano@Rauls-MacBook-Air.local>
2021-10-15 20:55:49 -0230

    Refactored code to resemble LS solution structure

--- lesson_5/guess-a-word/javascripts/game.js
+++ lesson_5/guess-a-word/javascripts/game.js
@@ -1,31 +1,10 @@
-/*
-PROBLEM
-  - We'll need a function `randomWord` that can pick one word out at a time randomly.
+document.addEventListener('DOMContentLoaded', () => {
+  const message = document.querySelector('#message');
+  const letters = document.querySelector('#spaces');
+  const guesses = document.querySelector('#guesses');
+  const apples = document.querySelector('#apples');
+  const replace = document.querySelector('#replay');
 
-RULES
-  - A picked word can't be picked again, and when all words are taken, return undefined
-  - Our function has to have access to the words array defined outside of it. With each call of the function, it mutates the array to remove the element it used.
-
-ALGORITHM
-  - Generate a random value between 1 and the max number of elements in the array
-  - Delete the element with the index that shares that random value and return it
-*/
-
-// let randomWord = (() => {
-//   let wordsArray = ['apple', 'banana', 'orange', 'pear'];
-  
-//   function getRandomIndex() {
-//     let max = wordsArray.length - 1;
-//     return Math.floor(Math.random() * (max + 1));
-//   }
-  
-//   return () => {
-//     let chosenIndex = getRandomIndex();
-//     return wordsArray.splice(chosenIndex, 1)[0];
-//   }
-// })();
-
-let Game = (() => {
   let randomWord = (() => {
     let wordsArray = ['apple', 'banana', 'orange', 'pear'];
     
@@ -40,49 +19,47 @@ let Game = (() => {
     }
   })();
 
-  function removeCurrentBlanks() {
-    let spaces = document.querySelector('#spaces');
-    let allSpans = spaces.querySelectorAll('span');
+  function Game() {
+    this.incorrect = 0;
+    this.letters_guessed = [];
+    this.totalAllowedWrongGuesses = 6;
+    this.correct_spaces = 0; // What is this for?
+    this.word = randomWord();
 
-    if (allSpans.length >= 1) {
-      [].forEach.call(allSpans, currentSpan => {
-        currentSpan.remove();
-      });
+    if (!this.word) {
+      this.displayMessage("Sorry, I've run out of words!");
+      return this;
     }
+    
+    this.word = this.word.split('');
+    this.init();
   }
 
-  function setBlanksForWordContainer(wordToGuess) {
-    removeCurrentBlanks();
-
-    let spaces = document.querySelector('#spaces');
-
-    for(let index = 0; index < wordToGuess.length; index += 1) {
-      let newSpan = document.createElement('span');
-      spaces.appendChild(newSpan);
-    }
-
-    console.log(wordToGuess);
-  }
-
-  function noMoreWords() {
-    let message = document.querySelector('#message');
-    message.textContent = "Sorry, I've run out of words!";
-  }
-
-  return function Game() {
-    this.wordToGuess = randomWord();
-
-    if (!this.wordToGuess) {
-      noMoreWords();
-    } else {
-      this.incorrectGuesses = 0;
-      this.lettersGuessed = [];
-      this.totalAllowedWrongGuesses = 6;
+  Game.prototype = {
+    createBlanks: function() {
+      this.removeBlanks();
   
-      setBlanksForWordContainer(this.wordToGuess);
+      for(let index = 0; index < this.word.length; index += 1) {
+        let newSpan = document.createElement('span');
+        letters.appendChild(newSpan);
+      }
+    },
+    removeBlanks: function() {
+      let allSpans = letters.querySelectorAll('span');
+  
+      if (allSpans.length >= 1) {
+        [].forEach.call(allSpans, currentSpan => {
+          currentSpan.remove();
+        });
+      }
+    },
+    displayMessage: function(text) {
+      message.textContent = text;
+    },
+    init: function() {
+      this.createBlanks();
     }
-  };
-})();
-
-
-new Game();
+  }
+  
+  new Game();
+});
