18c34fefa1ce2b15a33c1bffa32aa8ba5eb2c79e
Raul Romero Cano <raulromerocano@Rauls-MacBook-Air.local>
2022-04-25 21:31:47 -0230

    Re-organized contents

--- dom_exercises/2_events_async_programming/pr2.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('html').addEventListener('click', (event) => {
-  const container = document.querySelector('#container');
-
-  if (!container.contains(event.target)) {
-    container.style = 'display: none';
-  }
-});
--- dom_exercises/2_events_async_programming/pr5.js
+++
@@ -1,7 +0,0 @@
-let main = document.querySelector('main');
-
-main.addEventListener('contextmenu', event => {
-  event.preventDefault();
-  let name = event.target.textContent.trim().split(' ')[0];
-  alert(name);
-});
